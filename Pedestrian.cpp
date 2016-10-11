bbb1365eea28581c5264623593b4bb3ed013c736
Gunnar <agunnell2@student.cccs.edu>
2016-10-11 00:09:57 -0600

    Add files via upload

---
+++ Pedestrian.cpp
@@ -0,0 +1,13 @@
+#include "Pedestrian.h"
+
+
+Pedestrian::Pedestrian()
+{
+	setWidth(12); // inches
+	setLength(4);
+}
+
+
+Pedestrian::~Pedestrian()
+{
+}
---
+++ Pedestrian.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Includes.h"
+
+class Pedestrian : public TransportMode
+{
+public:
+	Pedestrian();
+	~Pedestrian();
+};
+
