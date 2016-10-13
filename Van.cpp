2bb73b85dc2868ef308ab4d9d4ce0aa2b3f9c848
Gunnar <agunnell2@student.cccs.edu>
2016-10-13 11:52:10 -0600

    Update Van.cpp

--- Van.cpp
+++ Van.cpp
@@ -1,20 +1,6 @@
 #include "Van.h"
 
-Van::Van()
-{
-	cout << "Default constructor for Van called.\n";
-
-	acceleration = 10.2;
-	maxSpeed = 94;
-	weight = 4700;
-	length = 200;			//inches
-	width = 80;				//inches
-	setOversized(false);
-	oversized = false;
-	model = VAN;
-	activeEmergency = false;
-}
-Van::Van(bool emergency) //JG optional, defaults to false
+Van::Van(bool emergency) //JG bool input optional, set to false if default constructor called
 {
 #if TSIM_CONSTRUCTOR_OUTPUT == 1
 	if (emergency)
@@ -28,7 +14,7 @@ Van::Van(bool emergency) //JG optional, defaults to false
 	//JG ambulance specs
 	if (emergency)
 	{
-		acceleration = 5.5;
+		acceleration = 5.5;  // seconds to 60 mph
 		maxSpeed = 100;
 		weight = 4700;
 		length = 180;			//inches
