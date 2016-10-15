db67e7fae3f20d9ee62b603a12b5c8bb97e3a827
spencemw <swilson246@student.cccs.edu>
2016-10-14 21:14:42 -0600

    Traffic Signal Class
    Hi guys.  I have added a traffic signal class file for our team to work on and complete.  The file is very raw and even lacks constructors so it will not run.  I have uploaded it mostly so you can see where we are and begin the process of rounding it out as a team.  Feel free to write any methods you think are germane.   Perhaps we should meet next week to discuss.   Spence.

---
+++ TrafficSignal.cpp
@@ -0,0 +1,80 @@
+#include "TrafficSignal.h"
+#include "includes.h"
+
+Signal::Signal()
+{
+}
+
+Signal::Signal(bool signControled)
+{
+	if (signControled == true)
+	{
+		fixedSign == true;
+		lightedSignal == false;
+		SignControlled(instance);
+	}
+
+	else
+	{
+		fixedSign == false;
+		lightedSignal == true;
+		ElecControlled(instance);
+	}
+}
+
+/****************************************************************************************
+Setters
+*****************************************************************************************/
+
+void Signal::setlatitude(double inputlatitude)
+{
+	if (inputlatitude <= MAXLAT && inputlatitude >= MINLAT)
+	{
+		latitude = inputlatitude;
+	}
+
+	else
+	{
+		cout << "Error void Signal::setlatitude(double inputlatitude) \n";
+	}
+}
+
+void Signal::setLongitude(double inputLongitude)
+{
+	if (inputLongitude <= MAXLONG && inputLongitude >= MINLONG)
+	{
+		longitude = inputLongitude;
+	}
+
+	else
+	{
+		cout << "Error void Signal::setLongitude(double inputLongitude) \n";
+	}
+
+}
+
+void Signal::setNumberOfPossDirections(int dir)
+{
+	if (dir > 0)
+	{
+		numberOfPossDirections = dir;
+	}
+
+	else
+	{
+		cout << "Error void Signal::setNumberOfPossDirections(int dir) \n";
+	}
+}
+
+void Signal::SignControlled : setIntersectionName(string str)
+{
+	intersectionName = str;
+}
+int signType;							//stop, yield, etc.
+int directionNumber;					//number the possible choices
+double controlledDirection;				//radians east is zero
+bool twoWayTraffic;						//true on two way streets
+
+Signal::~Signal()
+{
+}
