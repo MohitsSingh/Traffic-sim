6f9502fdfb461fd21ff31dc18e54658936ad9e4f
csc233 <mikeritter1993@gmail.com>
2016-10-22 13:38:41 -0600

    Update from master
    Saw you guys were using the old versions of MapOBJ so I updated everything for you guys. I also added some comments in main.cpp Ctrl f for Mike. Let me know if anything broke but everything should work fine.

--- MapOBJ.cpp
+++ MapOBJ.cpp
@@ -1,35 +1,20 @@
 #include "Includes.h"
 
+//Constructors
+MapOBJ::MapOBJ(int xStartCoordinate, int yStartCoordinate)
+{
+	xPos = xStartCoordinate;
+	yPos = yStartCoordinate;
+}
 
-//getters
-enum CARDINAL MapOBJ::getCardinalD()
+MapOBJ::MapOBJ()
 {
-	return cardinalD;
+	xPos = -1;
+	yPos = -1;
 }
-double MapOBJ::getMaxAcceleration()
-{
-	return maxAcceleration;
-}
-double MapOBJ::getbrakingPower()
-{
-	return brakingPower;
-}
-double MapOBJ::getAcceleration()
-{
-	return acceleration;
-}
-int MapOBJ::getMinSpeed()
-{
-	return minSpeed;
-}
-int MapOBJ::getMaxSpeed()
-{
-	return maxSpeed;
-}
-double MapOBJ::getCurrentSpeed()
-{
-	return currentSpeed;
-}
+
+
+//Getters
 int MapOBJ::getX()
 {
 	return xPos;
@@ -38,44 +23,102 @@ int MapOBJ::getY()
 {
 	return yPos;
 }
+double MapOBJ::getLatitude()
+{
+	return latitude;
+}
+double MapOBJ::getLongitude()
+{
+	return longitude;
+}
+int MapOBJ::getLength()
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return -1;
+}
+int MapOBJ::getWidth()
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return -1;
+}
+int MapOBJ::getWeight()
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return -1;
+}
+int MapOBJ::getTraction()
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return -1;
+}
+enum CARDINAL MapOBJ::getCardinalD()
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return ERROR;
+}
+double MapOBJ::getMaxAcceleration()
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return -1;
+}
+double MapOBJ::getbrakingPower()
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return -1;
+}
+double MapOBJ::getAcceleration()
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return -1;
+}
+int MapOBJ::getMinSpeed()
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return -1;
+}
+int MapOBJ::getMaxSpeed()
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return -1;
+}
+double MapOBJ::getCurrentSpeed()
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return -1;
+}
 double MapOBJ::getDirection()
 {
-	return direction;
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return -1;
 }
 int MapOBJ::getHazardRating()
 {
-	return hazardRating;
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return -1;
+}
+CARTYPES MapOBJ::getModel()
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return CARTYPES::UNDEFINED;
+}
+bool MapOBJ::getEmergencyVehicle()
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return nullptr;
+}
+bool MapOBJ::isActiveEmergency()		
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return nullptr;
+}
+bool MapOBJ::getOversized()
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+	return nullptr;
 }
 
-//setters
-void MapOBJ::setCardinalD(enum CARDINAL inputCardinalD)
-{
-	cardinalD = inputCardinalD;
-}
-void MapOBJ::setbrakingPower(double inputbrakingPower)
-{
-	brakingPower = inputbrakingPower;
-}
-void MapOBJ::setAcceleration(double inputAcceleration)
-{
-	acceleration = inputAcceleration;
-}
-void MapOBJ::setMaxAcceleration(double inputSpeed)
-{
-	maxAcceleration = inputSpeed;
-}
-void MapOBJ::setMinSpeed(int inputSpeed)
-{
-	minSpeed = inputSpeed;
-}
-void MapOBJ::setMaxSpeed(int inputSpeed)
-{
-	maxSpeed = inputSpeed;
-}
-void MapOBJ::setCurrentSpeed(double inputSpeed)
-{
-	currentSpeed = inputSpeed;
-}
+
+//Setters
 void MapOBJ::setX(int inputX)
 {
 	xPos = inputX;
@@ -84,11 +127,80 @@ void MapOBJ::setY(int inputY)
 {
 	yPos = inputY;
 }
+void MapOBJ::setLatitude(double input)
+{
+	latitude = input;
+}
+void MapOBJ::setLongitude(double input)
+{
+	longitude = input;
+}
+void MapOBJ::setCardinalD(enum CARDINAL inputCardinalD)
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+}
+void MapOBJ::setbrakingPower(double inputbrakingPower)
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+}
+void MapOBJ::setAcceleration(double inputAcceleration)
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+}
+void MapOBJ::setMaxAcceleration(double inputSpeed)
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+}
+void MapOBJ::setMinSpeed(int inputSpeed)
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+}
+void MapOBJ::setMaxSpeed(int inputSpeed)
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+}
+void MapOBJ::setCurrentSpeed(double inputSpeed)
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+}
 void MapOBJ::setDirection(double inputDirection)
 {
-	direction = inputDirection;
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
 }
 void MapOBJ::setHazardRating(int inputHazardRating)
 {
-	hazardRating = inputHazardRating;
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+}
+void MapOBJ::setLength(int inputLength)
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+}
+void MapOBJ::setWidth(int inputWidth)
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+}
+void MapOBJ::setTraction(int inputTraction)
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+}
+void MapOBJ::setOversized(bool inputOversized)
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+}
+void MapOBJ::setModel(CARTYPES inputModel)
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+}
+void MapOBJ::setEmergencyVehicle(bool inputEmergencyVehicle)
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+}
+void MapOBJ::setActiveEmergency(bool input)	
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+}
+void MapOBJ::setWeight(int input)
+{
+	cout << "\nyou somehow called a empty funcion in MapOBJ\n";
+
 }
--- MapOBJ.h
+++ MapOBJ.h
@@ -5,55 +5,60 @@
 class MapOBJ
 {
 protected:
-
-	
-	int minSpeed;				//minimum speed the car is allowed to go
-	int maxSpeed;				//maximum speed the car can go (speed limit on current rode)
 	int xPos;					//x position of object
 	int yPos;					//y position of object
-	double currentSpeed;		//the current speed of vehicle
-	double acceleration;		//current acceleration 
-	double maxAcceleration;		//maximum acceleration
-	double brakingPower;		//Vehicle spec of breaking power
 	double latitude;			//vehicle position North/South: relates to the y axis
 	double longitude;			//vehicle position East/West: relates to the x axis
-	double direction;			//radians: east is zero
-	enum CARDINAL cardinalD;	//direction of car for simple cardinal directions
-	
-
-
-	
-	int hazardRating;			//not being used at the moment
 
 public:
-	
 
-	
+	//Constructors
+	MapOBJ();
+	MapOBJ(int xStartCoordinate, int yStartCoordinate);
+
 	//GETTERS
 	virtual int getMinSpeed();
 	virtual int getMaxSpeed();
-	virtual int getHazardRating();
-	virtual double getMaxAcceleration();
-	virtual double getCurrentSpeed();
-	virtual double getbrakingPower();
-	virtual double getAcceleration();
-	virtual double getDirection();
 	virtual int getX();
 	virtual int getY();
-	virtual enum CARDINAL getCardinalD();
+	virtual int getLength();
+	virtual int getWidth();
+	virtual int getWeight();
+	virtual int getTraction();
+	virtual int getHazardRating();
+	virtual double getMaxAcceleration();
+	virtual double getAcceleration();
+	virtual double getbrakingPower();
+	virtual double getCurrentSpeed();
+	virtual double getDirection();
+	virtual double getLatitude();
+	virtual double getLongitude();
+	virtual bool getEmergencyVehicle();
+	virtual bool isActiveEmergency();			
+	virtual bool getOversized();
+	virtual CARTYPES getModel();
+	virtual CARDINAL getCardinalD();
 	
-	
-
 	//SETTERS
-	virtual void setMaxAcceleration(double inputSpeed);
 	virtual void setMinSpeed(int inputSpeed);
 	virtual void setMaxSpeed(int inputSpeed);
-	virtual void setCurrentSpeed(double inputSpeed);
-	virtual void setbrakingPower(double inputbrakingPower);
-	virtual void setHazardRating(int inputHazardRating);
-	virtual void setAcceleration(double inputAcceleration);
-	virtual void setDirection(double inputDirection);
 	virtual void setX(int inputX);
 	virtual void setY(int inputY);
-	virtual void setCardinalD(enum CARDINAL inputCardinalD);
+	virtual void setLength(int inputLength);
+	virtual void setWidth(int inputWidth);
+	virtual void setWeight(int input);
+	virtual void setTraction(int inputTraction);
+	virtual void setHazardRating(int inputHazardRating);
+	virtual void setMaxAcceleration(double inputSpeed);
+	virtual void setAcceleration(double inputAcceleration);
+	virtual void setbrakingPower(double inputbrakingPower);
+	virtual void setCurrentSpeed(double inputSpeed);
+	virtual void setDirection(double inputDirection);
+	virtual void setLatitude(double input);
+	virtual void setLongitude(double input);
+	virtual void setEmergencyVehicle(bool inputEmergencyVehicle);
+	virtual void setActiveEmergency(bool);			
+	virtual void setOversized(bool inputOversized);
+	virtual void setModel(CARTYPES inputModel);
+	virtual void setCardinalD(CARDINAL inputCardinalD);
 };
