0e6fc9633377b030832de1b25986e0e6d3dfa47a
TDWL-PDA <jiejie@rams.colostate.edu>
2016-12-06 13:29:22 -0700

    Update Van.cpp

--- Van.cpp
+++ Van.cpp
@@ -40,3 +40,42 @@ Van::Van(bool emergency) //JG bool input optional, set to false if default const
 Van::~Van()
 {
 }
+
+Van::Van(double x, double y, bool emergency)
+{
+#if TSIM_CONSTRUCTOR_OUTPUT == 1
+	if (emergency)
+	{
+		cout << "\"Emergency\" constructor for Van called.\n";
+	}
+	else
+		cout << "Default constructor for Van called.\n";
+#endif
+
+	// ambulance specs
+	if (emergency)
+	{
+		acceleration = 5.5;  		// seconds to 97kph (or 60mph)
+		maxSpeed = 164;			// kph
+		weight = 4700;			// pounds
+		length = 5;			// rounded meters
+		width = 2;			// rounded meters
+	}
+	// normal van specs
+	else
+	{
+		acceleration = 10.2;  		// seconds to 97kph (or 60mph)
+		maxSpeed = 151;			// kph
+		weight = 4700;			// pounds
+		length = 5;			// rounded meters
+		width = 2;			// rounded meters
+	}
+
+	model = VAN;
+	oversized = false;
+	activeEmergency = false;
+	emergencyVehicle = emergency;
+
+	setX(x);
+	setY(y);
+}
