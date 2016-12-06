9bb4298981be745cd76c5632965a22765031bbf9
Gunnar <agunnell2@student.cccs.edu>
2016-12-05 22:19:08 -0700

    Update Semi.cpp

--- Semi.cpp
+++ Semi.cpp
@@ -5,11 +5,11 @@ Semi::Semi()
 {
 	cout << "Default constructor for Semi called.\n";
 
-	setAcceleration(59);  //units?
-	setMaxSpeed(74);  //units?
-	setWeight(55000); //units?
-	setLength(21); // meters
-	setWidth(3); // meters
+	setAcceleration(59);   		// seconds to 97kph
+	setMaxSpeed(119);  		// kph
+	setWeight(55000); 		// pounds
+	setLength(21); 			// meters
+	setWidth(3); 			// meters
 	setOversized(true);
 }
 
