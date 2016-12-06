2457c9f94b2d9fa5d024c9b0c49321e764bf64af
Gunnar <agunnell2@student.cccs.edu>
2016-12-06 13:57:11 -0700

    Update Car.cpp
    Fixed incorrect comments and values

--- Car.cpp
+++ Car.cpp
@@ -43,25 +43,25 @@ Car::~Car()
 
 
 //Constructor where you can set the x and y values.
-Car::Car(double x, double y, bool emergency)		//JG bool input optional, defaults to false if default constructor called
+Car::Car(double x, double y, bool emergency)		
 {
-	//JG police car specs (probably needs tweaking)
+	// police car specs
 	if (emergency)
 	{
-		acceleration = 4.5;  // seconds to 60mph
-		maxSpeed = 180;
-		weight = 4000;
-		length = 200;			//inches
-		width = 85;				//inches
+		acceleration = 4.5;  	// seconds to 97kph (or 60mph)
+		maxSpeed = 290;  	// kph
+		weight = 4000;		// pounds
+		length = 5;		// rounded meters
+		width = 2;		// rounded meters
 	}
-	//JG normal car specs
+	// normal car specs
 	else
 	{
-		acceleration = 7.5;
-		maxSpeed = 150;
-		weight = 4000;
-		length = 6;			//inches
-		width = 4;				//inches
+		acceleration = 7.5;	// seconds to 97kph (or 60mph)
+		maxSpeed = 240;		// kph
+		weight = 4000;		// pounds
+		length = 5;		// rounded meters
+		width = 2;		// rounded meters
 	}
 
 	model = MIDSIZE;
