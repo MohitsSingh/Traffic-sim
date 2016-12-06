87620fdf73b274905c891cfaa68045e138865c68
TDWL-PDA <jiejie@rams.colostate.edu>
2016-12-06 13:26:04 -0700

    Update Semi.cpp

--- Semi.cpp
+++ Semi.cpp
@@ -16,3 +16,18 @@ Semi::Semi()
 Semi::~Semi()
 {
 }
+
+Semi::Semi(double x, double y)
+{
+	cout << "Default constructor for Semi called.\n";
+
+	setAcceleration(59);   		// seconds to 97kph
+	setMaxSpeed(119);  		// kph
+	setWeight(55000); 		// pounds
+	setLength(21); 			// meters
+	setWidth(3); 			// meters
+	setOversized(true);
+
+	setX(x);
+	setY(y);
+}
