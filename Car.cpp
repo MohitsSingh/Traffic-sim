6f3dbf264e17b790d4d284af76aafefdc51659b3
TDWL-PDA <jiejie@rams.colostate.edu>
2016-12-06 13:25:30 -0700

    Update Car.cpp

--- Car.cpp
+++ Car.cpp
@@ -40,3 +40,35 @@ Car::Car(bool emergency)		//JG bool input optional, defaults to false if default
 Car::~Car()
 {
 }
+
+
+//Constructor where you can set the x and y values.
+Car::Car(double x, double y, bool emergency)		//JG bool input optional, defaults to false if default constructor called
+{
+	//JG police car specs (probably needs tweaking)
+	if (emergency)
+	{
+		acceleration = 4.5;  // seconds to 60mph
+		maxSpeed = 180;
+		weight = 4000;
+		length = 200;			//inches
+		width = 85;				//inches
+	}
+	//JG normal car specs
+	else
+	{
+		acceleration = 7.5;
+		maxSpeed = 150;
+		weight = 4000;
+		length = 6;			//inches
+		width = 4;				//inches
+	}
+
+	model = MIDSIZE;
+	oversized = false;
+	activeEmergency = false;
+	emergencyVehicle = emergency;
+
+	setX(x);
+	setY(y);
+}
